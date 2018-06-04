e9d96af8e163dda8aab885ccfce8ddf9212074d4
zoeelarson01 <zoeelarson@gmail.com>
2018-06-04 21:52:50 +0000

    automatically backed up by learn

---
+++ about_me.rb
@@ -0,0 +1,8 @@
+def about_me
+  puts "My name is Zoë"
+  puts "I am from South Florida"
+  puts "I am 16 years old"
+  puts "My favorite ice cream flavor is chocolate peanut butter"
+end
+
+about_me
